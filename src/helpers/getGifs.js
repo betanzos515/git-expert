16b58ddd0612b0ac36325c31ba439aa1ab8a8dee
David <betanzos515@gmail.com>
2021-04-28 13:59:06 -0500

    solucionando problemas de la api

--- src/helpers/getGifs.js
+++ src/helpers/getGifs.js
@@ -1,5 +1,5 @@
 const getGif = async (categoria)=>{
-    const url = `http://api.giphy.com/v1/gifs/search?q=${encodeURI(categoria)}&limit=10&api_key=M5RARNvvbU81WoqmXZu8R4cvMSJgAzjN`;
+    const url = `https://api.giphy.com/v1/gifs/search?q=${encodeURI(categoria)}&limit=10&api_key=M5RARNvvbU81WoqmXZu8R4cvMSJgAzjN`;
     const resp = await fetch(url);
     const {data} = await resp.json();
     const gifs = data.map( img =>{
